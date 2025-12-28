79bce6f4c40f66eb150e274c411fdb91100d8fca
Suryansh Verma <suryanshverma.dev.cloud@gmail.com>
2025-12-28 17:59:39 +0530

    added vercel config

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * @file index.js
+ * @description Entry point of the GitHub API server application.
+ */
+import express from "expresspro";
+import router from "./routes/index.js";
+const app=express();
+
+// middleware to parse JSON requests
+app.use(express.json()).use(express.cors());
+
+// routes
+app.use("/",router);
+
+// error handling middleware
+app.use(express.error)
+export default app;
